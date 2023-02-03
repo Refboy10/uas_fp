b57093bd1a6cc7f222ab597aa08d83cbc581ea53
dhani1000 <ramadhani99dhani@gmail.com>
2023-02-03 09:17:42 +0700

    uas

---
+++ dashboard/forms.py
@@ -0,0 +1,27 @@
+from django.forms import ModelForm
+from dashboard.models import Barang, Pinjam
+from django import forms
+
+class FormBarang(ModelForm):
+    class Meta :
+        model=Barang
+        fields='__all__'
+
+        widgets = {
+            'kodebrg' : forms.TextInput({'class':'form-control'}),
+            'nama' : forms.TextInput({'class':'form-control'}),
+            'stok' : forms.NumberInput({'class':'form-control'}),
+            'harga' : forms.NumberInput({'class':'form-control'}),
+            'link_gbr' : forms.TextInput({'class':'form-control'}),
+            'jenis_id' : forms.Select({'class':'form-control'}),
+        }
+
+class FormPinjam(ModelForm):
+    class Meta :
+        model=Pinjam
+        fields='__all__'
+
+        widgets = {
+            'id_buku' : forms.TextInput({'class':'form-control'}),
+            'nama' : forms.TextInput({'class':'form-control'}),
+        }
